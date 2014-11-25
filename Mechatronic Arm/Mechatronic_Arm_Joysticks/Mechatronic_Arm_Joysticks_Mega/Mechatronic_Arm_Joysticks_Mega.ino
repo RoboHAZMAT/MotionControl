34a632f2db93a1e3f9b46eb0a6e9f858bd788003
JohnGardiner93 <john.gardiner93@gmail.com>
2014-11-25 00:47:32 -0500

    Mechatronic Arm Update
    Uno
    Configured I2C recieve
    May reconfigure as Master
    Meant to read servo positions in array and write to servos
    
    Mega
    Testing joysticks
    Wrote LED feedback
    Dealing with floating Joystick button value
    Next Step is to configure I2C send, probably as slave

--- Mechatronic Arm/Mechatronic_Arm_Joysticks/Mechatronic_Arm_Joysticks_Mega/Mechatronic_Arm_Joysticks_Mega.ino
+++ Mechatronic Arm/Mechatronic_Arm_Joysticks/Mechatronic_Arm_Joysticks_Mega/Mechatronic_Arm_Joysticks_Mega.ino
@@ -1,4 +1,3 @@
-#include <Servo.h>    // Use Servo library
 //Version 1.0
 //Date: 11/16/2014
 //
@@ -10,39 +9,147 @@
 //Equipment: Arduino Mega
 
 ///////////////////////////////////////////////////////////////////////////////
-
+#include <Wire.h>
 // Servos array in order from bottom of the arm up
 // Instantiate array
-// shoulderRotate : Servo 1 - motor[0]
-// shoulderBend   : Servo 2 - motor[1]
-// elbow          : Servo 3 - motor[2]
-// wristBend      : Servo 4 - motor[3]
-// wristRotate    : Servo 5 - motor[4]
-// gripper        : Servo 6 - motor[5]
-Servo motor[6];
-
-
+// shoulderRotate : Servo 1 - motor[0] | No Indicator | Trimpot Rotate | A0
+// shoulderBend   : Servo 2 - motor[1] | LEDs 2 & 3 | R. Joystick RL | A1
+// elbow          : Servo 3 - motor[2] | LEDs 4 & 5 | R. Joystick UD | A2
+// wristBend      : Servo 4 - motor[3] | LEDs 6 & 7 | L. Joystick UD | A3
+// wristRotate    : Servo 5 - motor[4] | LEDs 8 & 9 | L. Joystick RL | A4
+// gripper        : Servo 6 - motor[5] | LEDs 22 & 23| RL. Joystick Press | 36,37
+//
+int LEDS[] = {2,4,6,8,3,5,7,9,22,23,24};  // the number of the LED pin
+int BUTTONS[] = {36,37,38,39,40};
+long fade[10];
+long pos[6];
+int p = 0;
+int pp = 0;
+long curval;
+int button0 = 0;
+int button1 = 0;
 ///////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
-  //Connect servos to pwm pins
-  motor[0].attach(11);
-  motor[1].attach(10);
-  motor[2].attach(9);
-  motor[3].attach(6);
-  motor[4].attach(5);
-  motor[5].attach(3);
+// for (int p = 0; p < 11; p++)
+// {
+//   pinMode(LEDS[p],OUTPUT);
+// }
+// 
+// for (int pp = 0; pp < 5; pp++)
+// {
+//   pinMode(BUTTONS[pp],INPUT);
+// }
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(22,OUTPUT);
+  pinMode(23,OUTPUT);
+  pinMode(24,OUTPUT);
+  
+  pinMode(36,INPUT);
+  pinMode(37,INPUT);
+  pinMode(38,INPUT);
+  pinMode(39,INPUT);
+  pinMode(40,INPUT);
+ Serial.begin(9600);
+ 
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 void loop()
 {
-  for (int k = 0; k < 6; k++) {
-    motor[k].write(map(analogRead(k), 0, 1023, 0, 179));
+  for (int k = 0; k < 5; k++)
+  {
+    curval = analogRead(k);
+    if (k == 0)
+    {
+      pos[k] = map(curval, 0, 1023, 0, 179); //servo positioning
+      button0 = digitalRead(36);
+  button1 = digitalRead(37);
+  Serial.println(button0);
+  Serial.println(button1);
+  Serial.println("k=0");
+    }
+    else
+    {
+      button0 = digitalRead(36);
+  button1 = digitalRead(37);
+  Serial.println(button0);
+  Serial.println(button1);
+  Serial.println("k>0");
+  Serial.println(k);
+      pos[k] = map(curval, 0, 1023, 179, 0); //servo positioning
+      
+      if (curval < 500)
+      {
+        analogWrite(LEDS[(k-1)],map(curval, 0, 509, 255, 0));
+        analogWrite(LEDS[(k+3)],0);
+        button0 = digitalRead(36);
+  button1 = digitalRead(37);
+  Serial.println(button0);
+  Serial.println(button1);
+      }
+      else if (curval > 530)
+      {
+        analogWrite(LEDS[(k-1)],0);
+        analogWrite(LEDS[(k+3)],map(curval, 513, 1023, 0, 255));
+        button0 = digitalRead(36);
+  button1 = digitalRead(37);
+  Serial.println(button0);
+  Serial.println(button1);
+      }
+      else
+      {
+        analogWrite(LEDS[(k-1)],0);
+        analogWrite(LEDS[(k+3)],0);
+        button0 = digitalRead(36);
+  button1 = digitalRead(37);
+  Serial.println(button0);
+  Serial.println(button1);
+      }
+    }
   }
-  delay(50);
+  
+  delay(10);
+  
+  button0 = digitalRead(36);
+  button1 = digitalRead(37);
+  Serial.println(button0);
+  Serial.println(button1);
+  Serial.println("------");
+  delay(10000);
+  
+  if (button0 == LOW)
+  {
+    digitalWrite(LEDS[8],HIGH);
+    //Serial.println("Button 0 Low");
+  }
+  else
+  {
+    digitalWrite(LEDS[8],LOW);
+    //Serial.println("Button 0 High");
+  }
+  
+  if (button1 == LOW)
+  {
+    digitalWrite(LEDS[9],HIGH);
+    //Serial.println("Button 1 Low");
+  }
+  else
+  {  
+    digitalWrite(LEDS[9],LOW);
+    /////Serial.println("Button 1 High");
+  }
+
+ delay(10);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- Mechatronic Arm/Mechatronic_Arm_Joysticks/Mechatronic_Arm_Joysticks_Uno/Mechatronic_Arm_Joysticks_Uno.ino
+++ Mechatronic Arm/Mechatronic_Arm_Joysticks/Mechatronic_Arm_Joysticks_Uno/Mechatronic_Arm_Joysticks_Uno.ino
@@ -1,18 +1,21 @@
-#include <Servo.h>    // Use Servo library
-//Version 1.0
-//Date: 11/16/2014
+//Version 1.1
+//Date: 11/23/2014
 //
 // Description: For controlling mechatronic arm with joysticks. It features 6 servos controlled by PWM.
 // Uses 2 Sainsmart Joysticks. Joysticks have 2 trimpots each and one button each. Each trimpot is used for
 // one servo. The two buttons are used for one servo. Rotation of the base is handled by a fifth trimpot.
 // This portion of the code handles the movement of the servos. The Mega portion of the code handles the 
-// joystick input and the led feedback.
+// joystick input and the led feedback. This version uses pure I2C. However, wireless communication wlll
+// not be possible using I2C and wireless serial communication will be needed in the future.
 //
-// Desired Future Updates: Optimization. Mode switch. Wireless serial communication
+// Desired Future Updates: Optimization. Control Mode switch. Wireless serial communication. Comm mode switch.
 //
-// Equipment: Arduino Uno
+// Equipment: Arduino Uno, 2 4.7k ohm resisitors (for pull-up on GND and +5V lines)
 
 ///////////////////////////////////////////////////////////////////////////////
+//Libraries
+#include <Servo.h>  //Servo library
+#include <Wire.h>   //I2C library
 
 // Servos array in order from bottom of the arm up
 // shoulderRotate : Servo 1 - motor[0]
@@ -21,14 +24,20 @@
 // wristBend      : Servo 4 - motor[3]
 // wristRotate    : Servo 5 - motor[4]
 // gripper        : Servo 6 - motor[5]
-// Instantiate array
 
+// Instantiate arrays (servo and position)
 Servo motor[6];
+long rd[6];
+int p = 0;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
+  //I2C setup
+  Wire.begin(30);    //join I2C bus with address #30 
+  Wire.onReceive(receiveEvent); //register event
+  
   //Connect servos to pwm pins
   motor[0].attach(11);
   motor[1].attach(10);
@@ -36,6 +45,10 @@ void setup()
   motor[3].attach(6);
   motor[4].attach(5);
   motor[5].attach(3);
+  
+  //I2C
+ long rd[] = {90,90,90,90,90,90}; //initiate array at resting position in case comm does
+                           //not start right away
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -44,10 +57,22 @@ void loop()
 {
   
 //Loops through each servo reading (except the gripper which is handled digitally)
-  for (int k = 0; k < 5; k++) {
-    motor[k].write(map(analogRead(k), 0, 1023, 0, 179));
+  for (int k = 0; k < 6; k++) {
+    motor[k].write(rd[k]);
   }
-  delay(50);
+  delay(15);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+
+void receiveEvent(int howMany)
+{
+  p=0; //counting variable
+  while(Wire.available() > 0)
+  {
+    rd[p] = Wire.read();
+    p = p+1;
+  }
+  // intended to recieve an array indicating the positions of the servos
+  
+}
